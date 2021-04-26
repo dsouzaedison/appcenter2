4858e20ba263833366cf9d37acbd25c139272503
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-26 14:23:31 +0530

    Appcenter post build script added

---
+++ appcenter-post-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+IPAFile=$(find . -name "*.ipa" | head -1)
+
+npm install -g appcenter-cli
+
+appcenter login --token "568cc4593d1f96eac42b595a4df7e4d68af5b29b"
+
+appcenter distribute release -f "$IPAFile" -g Collaborators --app Hidden-Brains/appCenter-iOS
