ce61458c69d84e759f12f3ebc550c9d3dc8c0398
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-26 15:38:11 +0530

    Appcenter android post-build script added

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 IPAFile=$(find . -name "*.ipa" | head -1)
+APKFile=$(find . -name "*.apk" | head -1)
 
 npm install -g appcenter-cli
 
 appcenter login --token "568cc4593d1f96eac42b595a4df7e4d68af5b29b"
 
 appcenter distribute release -f "$IPAFile" -g Collaborators --app Hidden-Brains/appCenter-iOS
+appcenter distribute release -f "$APKFile" -g Collaborators --app Hidden-Brains/appCenter-Android
