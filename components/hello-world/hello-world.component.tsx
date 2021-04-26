d822e577cf743268b320bfde1cd92f296c4ee855
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-26 14:18:32 +0530

    Hello World component added

---
+++ components/hello-world/hello-world.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const HelloWorld: React.FunctionComponent = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
