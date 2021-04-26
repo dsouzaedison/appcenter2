9d257297e1907a6ca6a8edb134fbd018a0091ce0
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-26 17:21:29 +0530

    post-build script - Publish apk, ipa if exists

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -7,5 +7,10 @@ npm install -g appcenter-cli
 
 appcenter login --token "568cc4593d1f96eac42b595a4df7e4d68af5b29b"
 
+if [ -n "$IPAFile" ]; then
 appcenter distribute release -f "$IPAFile" -g Collaborators --app Hidden-Brains/appCenter-iOS
+fi
+
+if [ -n "$APKFile" ]; then
 appcenter distribute release -f "$APKFile" -g Collaborators --app Hidden-Brains/appCenter-Android
+fi
