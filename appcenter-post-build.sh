cf93fff571a14ede72b9f69f2dbe30e89696d753
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-29 14:31:57 +0530

    Test appcenter env access on prebuild

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -9,7 +9,6 @@ npm install -g appcenter-cli
 
 echo "Appcenter token 1: ${APP_CENTER_TOKEN}"
 echo "Appcenter token 2: $(APP_CENTER_TOKEN)"
-echo "Appcenter token 3: $($APP_CENTER_TOKEN)"
 appcenter login --token "${APP_CENTER_TOKEN}"
 
 # APPCENTER_BRANCH - Verify this before distribution to ensure, only develop and master branch builds are distributed
@@ -17,7 +16,7 @@ appcenter login --token "${APP_CENTER_TOKEN}"
 if [ "$APP_CENTER_CURRENT_PLATFORM" == "ios" ] && [ -n "$ipaPath" ]; then
   cd "$ipaPath" || exit
   ipaPath=$(find . -name "*.ipa" | head -1)
-  mv "$ipaPath" "$($newName).ipa"
+  mv "$ipaPath" "$(newName).ipa"
   cd "$pwa" || exit
   ipaPath=$(find . -name "*.ipa" | head -1)
   appcenter distribute release -f "$ipaPath" -g Collaborators --app Hidden-Brains/appCenter-iOS
@@ -26,7 +25,7 @@ fi
 if [ "$APP_CENTER_CURRENT_PLATFORM" == "android" ] && [ -n "$apkPath" ]; then
   cd "$apkPath" || exit                                                                       # Move to apk file folder
   apkPath=$(find . -name "*.apk" | head -1)                                                   # Find apk filename
-  mv "$apkPath" "$($newName).apk"                                                             # Rename apk with custom name
+  mv "$apkPath" "$(newName).apk"                                                             # Rename apk with custom name
   cd "$pwa" || exit                                                                           # Get back to project root
   apkPath=$(find . -name "*.apk" | head -1)                                                   # Find the apk path from project root
   appcenter distribute release -f "$apkPath" -g Collaborators --app dsouzaedison11/appCenter2 # Distribute via appcenter
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running Prebuild..."
-echo "Appcenter token 2: $(APP_CENTER_TOKEN)"
-echo "Appcenter token 3: $($APP_CENTER_TOKEN)"
+echo "Appcenter env: " "$VERSION_NAME"
+echo "Appcenter token: $(APP_CENTER_TOKEN)"
 
 curl -fsSL https://horusec.io/bin/install.sh | bash && horusec start -p . --disable-docker="true"
