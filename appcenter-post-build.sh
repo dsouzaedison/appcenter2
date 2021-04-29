5dd406c945726fca9f517c77389b010c6754f84e
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-29 16:13:07 +0530

    Bug fix

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -12,7 +12,8 @@ appcenter login --token "568cc4593d1f96eac42b595a4df7e4d68af5b29b"
 
 # APPCENTER_BRANCH - Verify this before distribution to ensure, only develop and master branch builds are distributed
 
-if [ "$APP_CENTER_CURRENT_PLATFORM" == "ios" ] && [ -n "$ipaPath" ]; then
+#if [ "$APP_CENTER_CURRENT_PLATFORM" == "ios" ] && [ -n "$ipaPath" ]; then
+if [ -n "$ipaPath" ]; then
   cd "$ipaPath" || exit
   ipaPath=$(find . -name "*.ipa" | head -1)
   mv "$ipaPath" "$(newName).ipa"
@@ -21,7 +22,8 @@ if [ "$APP_CENTER_CURRENT_PLATFORM" == "ios" ] && [ -n "$ipaPath" ]; then
   appcenter distribute release -f "$ipaPath" -g Collaborators --app Hidden-Brains/appCenter-iOS
 fi
 
-if [ "$APP_CENTER_CURRENT_PLATFORM" == "android" ] && [ -n "$apkPath" ]; then
+#if [ "$APP_CENTER_CURRENT_PLATFORM" == "android" ] && [ -n "$apkPath" ]; then
+if [ -n "$apkPath" ]; then
   cd "$apkPath" || exit                                                                       # Move to apk file folder
   apkPath=$(find . -name "*.apk" | head -1)                                                   # Find apk filename
   mv "$apkPath" "$(newName).apk"                                                              # Rename apk with custom name
