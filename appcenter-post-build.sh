726f1cf8d8e9af1f5cac54c815793f8fd7467392
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-29 16:05:25 +0530

    Token update - appcenter login

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -7,7 +7,7 @@ timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 newName="custom_build_$($timestamp)"
 npm install -g appcenter-cli
 
-appcenter login --token "bf161c310998d8a0398958d0bb0f5d4fe14b5abb"
+appcenter login --token "568cc4593d1f96eac42b595a4df7e4d68af5b29b"
 #appcenter login --token "${APP_CENTER_TOKEN}"
 
 # APPCENTER_BRANCH - Verify this before distribution to ensure, only develop and master branch builds are distributed
@@ -24,7 +24,7 @@ fi
 if [ "$APP_CENTER_CURRENT_PLATFORM" == "android" ] && [ -n "$apkPath" ]; then
   cd "$apkPath" || exit                                                                       # Move to apk file folder
   apkPath=$(find . -name "*.apk" | head -1)                                                   # Find apk filename
-  mv "$apkPath" "$(newName).apk"                                                             # Rename apk with custom name
+  mv "$apkPath" "$(newName).apk"                                                              # Rename apk with custom name
   cd "$pwa" || exit                                                                           # Get back to project root
   apkPath=$(find . -name "*.apk" | head -1)                                                   # Find the apk path from project root
   appcenter distribute release -f "$apkPath" -g Collaborators --app dsouzaedison11/appCenter2 # Distribute via appcenter
