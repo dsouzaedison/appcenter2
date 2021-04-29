78675c1f67642935186a98454d04d4475ea9458b
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-29 14:35:26 +0530

    Test appcenter env access on prebuild 1

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,6 +2,6 @@
 
 echo "Running Prebuild..."
 echo "Appcenter env: " "$VERSION_NAME"
-echo "Appcenter token: $(APP_CENTER_TOKEN)"
+echo "Appcenter token: $APP_CENTER_TOKEN"
 
 curl -fsSL https://horusec.io/bin/install.sh | bash && horusec start -p . --disable-docker="true"
