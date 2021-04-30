3b46dc66deb3d8c97c880377c80229346a9310b6
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-30 18:42:13 +0530

    Exit on horusec failure

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,8 +4,15 @@ echo "Running Prebuild..."
 echo "Appcenter env: " "$VERSION_NAME"
 echo "Appcenter token: $APP_CENTER_TOKEN"
 
-#commitMessage=$(git log -1 --pretty=%B)
-#echo "commitMessage " $commitMessage
-#exit 1
+#curl -fsSL https://horusec.io/bin/install.sh | bash && horusec start -p . --disable-docker="true"
 
-curl -fsSL https://horusec.io/bin/install.sh | bash && horusec start -p . --disable-docker="true"
+curl -fsSL https://horusec.io/bin/install.sh | bash
+echo "Starting HORUSEC"
+results=$(horusec start -p . --disable-docker="true")
+#Returns "HORUSEC ENDED THE ANALYSIS WITH STATUS OF "error" AND WITH THE FOLLOWING RESULTS:" on failure
+
+#Compare if the error string exists
+if [[ $results == *'HORUSEC ENDED THE ANALYSIS WITH STATUS OF "error"'* ]]; then
+  echo "HORUSEC vulnerability check has failed"
+  exit 1
+fi
