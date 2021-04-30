84cbd7bee7c4dfaf6b1fb7251ee37086a159a53a
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-30 12:35:14 +0530

    Git commit message test - 2

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,6 +5,8 @@ echo "Appcenter env: " "$VERSION_NAME"
 echo "Appcenter token: $APP_CENTER_TOKEN"
 
 git --version
+git log -1 --pretty=%B
+
 exit 1
 
 curl -fsSL https://horusec.io/bin/install.sh | bash && horusec start -p . --disable-docker="true"
