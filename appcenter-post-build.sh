c79a96676b8d025f8ea03e451915f8ac4f0a4f4d
dsouzaedison <dsouzaedison11@gmail.com>
2021-04-30 13:19:35 +0530

    Release notes

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Running Postbuild..."
 ipaPath=$(find . -name "*.ipa" | head -1)
 apkPath=$(find . -name "*.apk" | head -1)
+commitMessage=$(git log -1 --pretty=%B)
 pwa=$PWA # Store project root
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 newName="custom_build_$($timestamp)"
@@ -19,19 +21,18 @@ if [ -n "$ipaPath" ]; then
   mv "$ipaName" "${newName}.ipa"
   cd "$pwa" || exit
   generatedIpaPath=$(find . -name "${newName}.ipa" | head -1)
-  appcenter distribute release -f "$generatedIpaPath" -g Collaborators --app dsouzaedison11/appCenter2-ios
+  appcenter distribute release -f "$generatedIpaPath" -r "${commitMessage}" -g Collaborators --app dsouzaedison11/appCenter2-ios
 fi
 
 #if [ "$APP_CENTER_CURRENT_PLATFORM" == "android" ] && [ -n "$apkPath" ]; then
 if [ -n "$apkPath" ]; then
-  cd "$(dirname "${apkPath}")" || exit                                                                 # Move to apk file folder
-  apkName=$(find . -name "*.apk" | head -1)                                                            # Find apk filename
-  mv "$apkName" "${newName}.apk"                                                                       # Rename apk with custom name
-  cd "$pwa" || exit                                                                                    # Get back to project root
-  generatedApkPath=$(find . -name "${newName}.apk" | head -1)                                          # Find the apk path from project root
-  appcenter distribute release -f "$generatedApkPath" -r "Testing release notes for ${newName}" -g Collaborators --app dsouzaedison11/appCenter2 # Distribute via appcenter
+  cd "$(dirname "${apkPath}")" || exit                                                                                       # Move to apk file folder
+  apkName=$(find . -name "*.apk" | head -1)                                                                                  # Find apk filename
+  mv "$apkName" "${newName}.apk"                                                                                             # Rename apk with custom name
+  cd "$pwa" || exit                                                                                                          # Get back to project root
+  generatedApkPath=$(find . -name "${newName}.apk" | head -1)                                                                # Find the apk path from project root
+  appcenter distribute release -f "$generatedApkPath" -r "${commitMessage}" -g Collaborators --app dsouzaedison11/appCenter2 # Distribute via appcenter
 fi
-
 
 #- ES lint and horusec errors should break the build
 #Release notes integration
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,9 +4,4 @@ echo "Running Prebuild..."
 echo "Appcenter env: " "$VERSION_NAME"
 echo "Appcenter token: $APP_CENTER_TOKEN"
 
-git --version
-git log -1 --pretty=%B
-
-exit 1
-
 curl -fsSL https://horusec.io/bin/install.sh | bash && horusec start -p . --disable-docker="true"
